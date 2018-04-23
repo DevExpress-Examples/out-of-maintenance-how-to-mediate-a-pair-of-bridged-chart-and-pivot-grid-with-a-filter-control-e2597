a662402ff8d7df0d2ef62a34f79e6311f5bb83df
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChartPivotInteractionApproaches
+{
+    public partial class Form2 : Form
+    {
+        private DataTable browsableTable;
+
+        public DataTable BrowsableTable
+        {
+            get { return browsableTable; }
+            set
+            {
+                browsableTable = value;
+                dataGridView1.DataSource = value;
+                RecalculateActualWidth();
+            }
+        }
+        
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_ColumnWidthChanged(object sender, DataGridViewColumnEventArgs e)
+        {
+            RecalculateActualWidth();
+        }
+
+        private void dataGridView1_RowHeadersWidthChanged(object sender, EventArgs e)
+        {
+            RecalculateActualWidth();
+        }
+
+        private void RecalculateActualWidth()
+        {
+            int totalWidth = 0;
+
+            foreach (DataGridViewColumn col in dataGridView1.Columns)
+                totalWidth += col.Width;
+
+            totalWidth += dataGridView1.RowHeadersWidth;
+
+            Width = totalWidth + 60;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            //base.OnClosing(e);
+            e.Cancel = true;
+            Hide();
+        }
+
+    }
+}
